ea2439f87cc6bec38e1d00233f758e1f239c1cfd
Vinod <vinod.kottamasu@upshot.ai>
2020-04-17 14:52:40 +0530

    resolved push click event

--- src/android/UpshotFirebaseMessagingService.java
+++ src/android/UpshotFirebaseMessagingService.java
@@ -138,14 +138,13 @@ public class UpshotFirebaseMessagingService extends FirebaseMessagingService {
         }
 
 
-        Intent intent = new Intent(this, mainActivity);
-        intent.putExtra("push", true);
-        intent.putExtra("payload", jsonObject.toString());
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
-
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
-                PendingIntent.FLAG_UPDATE_CURRENT);
+        Intent notifyIntent = new Intent(this, mainActivity);
+        notifyIntent.putExtra("push", true);
+        notifyIntent.putExtra("payload", jsonObject.toString());
+// Create the PendingIntent
+        PendingIntent pendingIntent = PendingIntent.getActivity(
+                this, notificationId, notifyIntent, PendingIntent.FLAG_UPDATE_CURRENT
+        );
 
         Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
 
@@ -190,16 +189,14 @@ public class UpshotFirebaseMessagingService extends FirebaseMessagingService {
                 } catch (JSONException e) {
                 }
 
-                Intent intent = new Intent(context, mainActivity);
-                intent.putExtra("push", true);
-                intent.putExtra("payload", jsonObject.toString());
+                Intent notifyIntent = new Intent(context, mainActivity);
+                notifyIntent.putExtra("push", true);
+                notifyIntent.putExtra("payload", jsonObject.toString());
+// Create the PendingIntent
+                PendingIntent pendingIntent = PendingIntent.getActivity(
+                        context, notificationId, notifyIntent, PendingIntent.FLAG_UPDATE_CURRENT
+                );
 
-                Log.i("jsonObject", jsonObject.toString() + ",gifNotificationId :  " + notificationId);
-
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent,
-                        PendingIntent.FLAG_UPDATE_CURRENT);
                 remoteViews.setOnClickPendingIntent(R.id.root_push_layout, pendingIntent);
 
                 Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
