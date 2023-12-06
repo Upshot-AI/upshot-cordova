cd574c9c043bc49b0e42d609bdf09d7bed89f21d
Vinod Kottamasu <vinod.kottamasu@xcubelabs.com>
2023-12-06 14:51:24 +0530

    updated gradle file

--- src/android/UpshotReviewManager.java
+++ src/android/UpshotReviewManager.java
@@ -3,32 +3,32 @@ package cordova_plugin_upshotplugin;
 import android.app.Activity;
 import android.content.Context;
 
+import com.google.android.gms.tasks.Task;
 import com.google.android.play.core.review.ReviewInfo;
 import com.google.android.play.core.review.ReviewManager;
 import com.google.android.play.core.review.ReviewManagerFactory;
-import com.google.android.play.core.tasks.Task;
 
 public class UpshotReviewManager {
 
   private ReviewManager reviewManager;
 
-    UpshotReviewManager(Context context) {
-      reviewManager = ReviewManagerFactory.create(context);
-    }
+  UpshotReviewManager(Context context) {
+    reviewManager = ReviewManagerFactory.create(context);
+  }
 
-    public void  showRateApp(Activity activity) {
+  public void showRateApp(Activity activity) {
 
-      Task<ReviewInfo> request = reviewManager.requestReviewFlow();
-      request.addOnCompleteListener(task -> {
+    Task<ReviewInfo> request = reviewManager.requestReviewFlow();
+    request.addOnCompleteListener(task -> {
 
-        if (task.isSuccessful()) {
-          ReviewInfo reviewInfo = task.getResult();
-          Task<Void> flow = reviewManager.launchReviewFlow(activity, reviewInfo);
-          flow.addOnCompleteListener(task1 -> {
+      if (task.isSuccessful()) {
+        ReviewInfo reviewInfo = task.getResult();
+        Task<Void> flow = reviewManager.launchReviewFlow(activity, reviewInfo);
+        flow.addOnCompleteListener(task1 -> {
 
-          });
-        }
-      });
+        });
+      }
+    });
 
-    }
+  }
 }
